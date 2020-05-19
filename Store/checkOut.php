c66c8c19f1b8dff38aac53e3d558e18cb5bcece4
Nwestfield <62273315+Nwestfield@users.noreply.github.com>
2020-05-18 19:52:18 -0700

    Update checkOut.php
    query checks out in the syntax checker now, still cant really test it though

--- Store/checkOut.php
+++ Store/checkOut.php
@@ -7,10 +7,10 @@ $dbname = "beslaDB";
 $conn=new mysqli($servername,$username,$password,$dbname);
 //my understanding is that the below is me inserting my information from my form into a table titled checkOut, and then for values im passing in the id of each text window.
 $sql= "INSERT INTO
-checkOut(customer_fname,customer_lname, customer_cc#,customer_email,customer_phone,customer_street,customer_city,
+checkOut(customer_fname,customer_lname, customer_cc,customer_email,customer_phone,customer_street,customer_city,
 customer_state,customer_country,customer_zip)
-VALUES('$_POST[fname]','$_POST[lname]','$_POST[Credit card #]','$_POST[email]','$_POST[phone]','$_POST[Street Address]',
-'$_POST[city]','$_POST[state]','$_POST[country]','$_POST[zip]')";
+VALUES('$_POST[fname]','$_POST[lname]','$_POST[Credit Card]','$_POST[Email]','$_POST[Phone number]','$_POST[Street Address]',
+'$_POST[City]','$_POST[State]','$_POST[Country]','$_POST[Zip Code]')";
 if($conn->query($sql) == TRUE) 
 {
   echo("One record added!");
